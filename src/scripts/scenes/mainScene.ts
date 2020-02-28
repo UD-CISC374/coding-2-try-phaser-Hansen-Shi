3f2bb5bedf85b33bb19a93d2ae02b5d78c0b3aeb
Hansen Shi <hansens@udel.edu>
2020-02-28 12:50:18 -0500

    Lots of things

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -1,16 +1,163 @@
-import ExampleObject from '../objects/exampleObject';
+import Beam from "./beam";
 
 export default class MainScene extends Phaser.Scene {
-  private exampleObject: ExampleObject;
+  background: Phaser.GameObjects.TileSprite;
+  
+  ship1: Phaser.GameObjects.Sprite;
+  ship2: Phaser.GameObjects.Sprite;
+  ship3: Phaser.GameObjects.Sprite;
+  player: Phaser.Physics.Arcade.Sprite;
+
+  boosts: Phaser.Physics.Arcade.Group;
+  
+  cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys;
+  spacebar: Phaser.Input.Keyboard.Key;
+  bomb: Phaser.Input.Keyboard.Key;
+  projectiles: Phaser.GameObjects.Group;
+  enemies: Phaser.Physics.Arcade.Group;
 
   constructor() {
     super({ key: 'MainScene' });
   }
 
-  create() {
-    this.exampleObject = new ExampleObject(this, 0, 0);
+  create() { 
+    this.background = this.add.tileSprite(0,0, 256, 272, "background");
+    this.background.setOrigin(0,0);
+    
+    this.ship1 = this.add.sprite(256/2 - 50, 272/2, "smol");
+    this.ship2 = this.add.sprite(256/2, 272/2, "norm")
+    this.ship3 = this.add.sprite(256/2 + 50, 272/2, "chonk")
+
+    this.player = this.physics.add.sprite(256/2 - 8, 272-64, "player")
+    this.player.play("player_anim");
+    this.cursorKeys = this.input.keyboard.createCursorKeys();
+    this.player.setCollideWorldBounds(true);
+    this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    this.bomb = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
+
+    this.projectiles = this.add.group();
+    
+    this.boosts = this.physics.add.group()
+
+    var maxBoosts = 3;
+    for (var i = 0; i <= maxBoosts; i++){
+      var boost = this.physics.add.sprite(16, 16, "power");
+      this.boosts.add(boost);
+      boost.setRandomPosition(0, 0, 256, 272);
+      
+      if(Math.random() > .25){
+      boost.play("bomb");
+    }
+      else {
+        boost.play("buddy");
+    }
+
+    boost.setVelocity(50, 50);
+    boost.setCollideWorldBounds(true);
+    boost.setBounce(1);
   }
 
+    this.ship1.play("smol_anim");
+    this.ship2.play("norm_anim");
+    this.ship3.play("chonk_anim");
+
+    this.enemies = this.physics.add.group();
+    this.enemies.add(this.ship1);
+    this.enemies.add(this.ship2);
+    this.enemies.add(this.ship3);
+
+    this.ship1.setInteractive();
+    this.ship2.setInteractive();
+    this.ship3.setInteractive();
+    this.player.setInteractive();
+
+    this.physics.add.overlap(this.player, this.boosts, this.pickUp);
+    this.physics.add.overlap(this.player, this.enemies, this.playerDeath);
+    this.physics.add.overlap(this.projectiles, this.enemies, this.hit);
+
+  }
+
+  pickUp(player, boost){
+    boost.disableBody(true, true);
+  }
+  playerDeath(player, enemy){
+    player.x = 256/2 - 8;
+    player.y = 272 - 30;
+  }
+  hit(projectile, enemy){
+    projectile.destroy();
+    enemy.destroy();
+  }
+  moveEnemy(ship: Phaser.GameObjects.Image, speed: integer){
+    ship.y += speed;
+    if (ship.y > 272){
+      this.spawnEnemy(ship);
+    }
+  }
+  movePlayer(){
+    if (this.cursorKeys.left?.isDown){
+      this.player.setVelocityX(-200);
+    }
+    else if (this.cursorKeys.right?.isDown){
+      this.player.setVelocityX(200);
+    }
+    else {
+      this.player.setVelocityX(0);
+    }
+    
+    if (this.cursorKeys.up?.isDown){
+      this.player.setVelocityY(-200);
+    }
+    else if (this.cursorKeys.down?.isDown){
+      this.player.setVelocityY(200);
+    }
+    else {
+      this.player.setVelocityY(0);
+    }
+  }
+  spawnEnemy(ship: Phaser.GameObjects.Image){
+    var random_x = Phaser.Math.Between(0, 256);
+    ship.y = 0;
+    ship.x = random_x;
+  }
+  spawnPowerUps(boost: Phaser.Physics.Arcade.Sprite){
+    if (boost.y > 272){
+      var random_y = Phaser.Math.Between(0, 272);
+      boost.x = 0;
+      boost.y = random_y;
+    }
+    if (boost.x > 256){
+      var random_y = Phaser.Math.Between(0,272);
+      boost.x = 0;
+      boost.y = random_y;
+    }
+  }
+  destroyEnemy(pointer, gameObject){
+    gameObject.setTexture("boom");
+    gameObject.play("boom_anim");
+  }
+  shoot(){
+    var beam = new Beam(this);
+  }
   update() {
+    this.moveEnemy(this.ship1, 3);
+    this.moveEnemy(this.ship2, 2);
+    this.moveEnemy(this.ship3, 1);
+
+    this.background.tilePositionY -= .5;
+    this.movePlayer();
+
+    if (Phaser.Input.Keyboard.JustDown(this.spacebar)){
+      this.shoot();
+    }
+    if (Phaser.Input.Keyboard.JustDown(this.bomb)){
+      console.log("drop em");
+    }
+
+    for (var i = 0; i < this.projectiles.getChildren().length; i++){
+      var beam = this.projectiles.getChildren()[i];
+      beam.update;
+    }
+
   }
 }
--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -4,9 +4,91 @@ export default class PreloadScene extends Phaser.Scene {
   }
 
   preload() {
+    this.load.image("background", "assets/images/background.png")
+    
+    this.load.spritesheet("smol", "assets/spritesheets/ship.png", {
+      frameWidth: 16,
+      frameHeight: 16
+    });
+    this.load.spritesheet("norm", "assets/spritesheets/ship2.png", {
+      frameWidth: 32,
+      frameHeight: 16
+    });
+    this.load.spritesheet("chonk", "assets/spritesheets/ship3.png", {
+      frameWidth: 32,
+      frameHeight: 32
+    });
+    this.load.spritesheet("boom", "assets/spritesheets/explosion.png", {
+      frameWidth: 16,
+      frameHeight: 16
+    });
+    this.load.spritesheet("power", "assets/spritesheets/power-up.png", {
+      frameWidth: 16,
+      frameHeight: 16
+    });
+    this.load.spritesheet("player", "assets/spritesheets/player.png", {
+      frameWidth: 16,
+      frameHeight: 24
+    });
+    this.load.spritesheet("beam", "assets/spritesheets/beam.png", {
+      frameWidth: 16,
+      frameHeight : 16
+    });
   }
 
   create() {
+    this.anims.create({
+      key: "smol_anim",
+      frames: this.anims.generateFrameNumbers("smol", {start: 0,end: 1}),
+      frameRate: 20,
+      repeat: -1
+    });
+    this.anims.create({
+      key: "norm_anim",
+      frames: this.anims.generateFrameNumbers("norm", {start: 0,end: 1}),
+      frameRate: 20,
+      repeat: -1
+    });
+    this.anims.create({
+      key: "chonk_anim",
+      frames: this.anims.generateFrameNumbers("chonk", {start: 0,end: 1}),
+      frameRate: 20,
+      repeat: -1
+    });
+    
+    this.anims.create({
+      key: "boom_anim",
+      frames: this.anims.generateFrameNumbers("boom", {start: 0,end: 4}),
+      frameRate: 20,
+      repeat: 0,
+      hideOnComplete: true
+    });
+
+    this.anims.create({
+      key: "buddy",
+      frames: this.anims.generateFrameNumbers("power", {start: 2, end: 3}),
+      frameRate: 20,
+      repeat: -1
+    });
+    this.anims.create({
+      key: "bomb",
+      frames: this.anims.generateFrameNumbers("power", {start: 1, end: 2}),
+      frameRate: 20,
+      repeat: -1
+    });
+    this.anims.create({
+      key: "player_anim",
+      frames: this.anims.generateFrameNumbers("player", {start: 0, end: 1}),
+      frameRate: 20,
+      repeat: -1
+    });
+    this.anims.create({
+      key: "beam_anim",
+      frames: this.anims.generateFrameNumbers("beam", {start: 0, end: 1}),
+      frameRate: 20,
+      repeat: -1
+    });
+    
     this.scene.start('MainScene');
   }
 }

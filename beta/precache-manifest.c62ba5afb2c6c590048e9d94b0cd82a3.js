af003a1546fa17b73d6f5cf18f96a3e8fa5cc9f0
Hansen Shi <hansens@udel.edu>
2020-02-28 14:45:51 -0500

    Updates

---
+++ beta/precache-manifest.c62ba5afb2c6c590048e9d94b0cd82a3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/coding-2-try-phaser-Hansen-Shi/betaassets/citations.md"
+  },
+  {
+    "revision": "9fd0dcea47820063efe7dabd12e32b87",
+    "url": "/coding-2-try-phaser-Hansen-Shi/betaassets/images/background.png"
+  },
+  {
+    "revision": "ccd0f68c9dc7cd68ea1e98bf43aa7c90",
+    "url": "/coding-2-try-phaser-Hansen-Shi/betaassets/images/ship.png"
+  },
+  {
+    "revision": "ea0ad2cda5a9dbbe332fd61e4c2f5700",
+    "url": "/coding-2-try-phaser-Hansen-Shi/betaassets/images/ship2.png"
+  },
+  {
+    "revision": "5896d72a8a287a6e5f3272bfb9eadca8",
+    "url": "/coding-2-try-phaser-Hansen-Shi/betaassets/images/ship3.png"
+  },
+  {
+    "revision": "6be3462bcaa4a183b678800bb66d176c",
+    "url": "/coding-2-try-phaser-Hansen-Shi/betaassets/spritesheets/beam.png"
+  },
+  {
+    "revision": "ea801e18a4f14b0c4740b555536b414e",
+    "url": "/coding-2-try-phaser-Hansen-Shi/betaassets/spritesheets/explosion.png"
+  },
+  {
+    "revision": "8c6cfd921c30ef330d7661d329d1aa90",
+    "url": "/coding-2-try-phaser-Hansen-Shi/betaassets/spritesheets/mothership.png"
+  },
+  {
+    "revision": "ff70e55fbb2b70941d98b3e7ab1a01aa",
+    "url": "/coding-2-try-phaser-Hansen-Shi/betaassets/spritesheets/player.png"
+  },
+  {
+    "revision": "05427110fd2c2de1d4738b31496a53c9",
+    "url": "/coding-2-try-phaser-Hansen-Shi/betaassets/spritesheets/power-up.png"
+  },
+  {
+    "revision": "6387e4bedb194c28efe573c1cc5ce303",
+    "url": "/coding-2-try-phaser-Hansen-Shi/betaassets/spritesheets/ship.png"
+  },
+  {
+    "revision": "4888982b102ce1a7c57fb9f8c129e63d",
+    "url": "/coding-2-try-phaser-Hansen-Shi/betaassets/spritesheets/ship2.png"
+  },
+  {
+    "revision": "15d30c88882683af9f5092c13dc13853",
+    "url": "/coding-2-try-phaser-Hansen-Shi/betaassets/spritesheets/ship3.png"
+  },
+  {
+    "revision": "9fa6bcf422831dc123d3",
+    "url": "/coding-2-try-phaser-Hansen-Shi/betagen.main.f4bb83770fa1409cea20.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-Hansen-Shi/betagen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "cc6e239e989661fba7cad5d819becaea",
+    "url": "/coding-2-try-phaser-Hansen-Shi/betaindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-Hansen-Shi/betamanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-Hansen-Shi/betasw.js"
+  }
+]);
--- beta/sw.js
+++ beta/sw.js
@@ -1,4 +1,4 @@
-importScripts("/educational-phaser-game-template/devprecache-manifest.549ef5a400021d602b1a18ffdc9f4103.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/coding-2-try-phaser-Hansen-Shi/betaprecache-manifest.c62ba5afb2c6c590048e9d94b0cd82a3.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.

fae7cd75cfc9068556a90bd06d68ea8c18023c15
Hansen Shi <hansens@udel.edu>
2020-02-28 22:55:38 -0500

    Updates

---
+++ dev/sw.js
@@ -0,0 +1,11 @@
+importScripts("/coding-2-try-phaser-Hansen-Shi/devprecache-manifest.10b72ef04a0295c59c59fad6db147bbd.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
